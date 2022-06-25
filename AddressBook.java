bae414dd4a08518a4a40af0ecc10ac312db910a4
rakeshrewale <rakeshrewale993@gmail.com>
2022-06-25 13:50:02 +0530

    Added: Welcome to addressbook system

---
+++ AddressBook.java
@@ -0,0 +1,9 @@
+package com.Addressbook;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		System.out.println("****** Welcome To The Address Book Program ******");
+	}
+
+}
